e9a867234596ddc21f6e36a5288dd50ac5c55888
hungnguyen <nvhung.bme@gmail.com>
2020-07-28 14:53:01 +0700

    add Option in SearchBar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import { notification } from 'antd';
 import { SmileOutlined, SearchOutlined } from '@ant-design/icons';
 import SearchBar from './Components/SearchBar';
-import YTsearch from 'youtube-api-search';
+import YTSearch from 'youtube-api-search';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const API_KEY = process.env.REACT_APP_API_KEY;
+// const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = 'AIzaSyAkrwpw9ZWj9Uc-4Va123UYx829drKEFSU';
 
 export default class App extends Component {
   constructor(props) {
@@ -18,24 +19,62 @@ export default class App extends Component {
       selectedVideo: {},
     };
     // this.welcome();
+    //this.videoSearch()
   }
-  // welcome = () => {
-  //   notification.open({
-  //     placement: 'topRight',
-  //     top: 30,
-  //     duration: 3,
-  //     rtl: true,
-  //     message: 'Hey, nice to see you here',
-  //     description: 'Let us start by searching some videos',
-  //     icon: <SmileOutlined style={{ color: '#108ee9' }} />
-  //   })
-  // }
+  welcome = () => {
+    notification.open({
+      placement: 'topRight',
+      top: 30,
+      duration: 3,
+      rtl: true,
+      message: 'Hey, nice to see you here',
+      description: 'Let us start by searching some videos',
+      icon: <SmileOutlined style={{ color: '#108ee9' }} />
+    })
+  }
+  
+  videoSearch = term => {
+      // fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&key=AIzaSyD8aYQqU-uJBBtM4n8GkhxMuK2XmWI6vh0')
+      //   .then(res => res.json())
+      //   .then(data => {
+      //     // console.log(data)
+      //     this.setState({videos: data.items, selectedVideo: data.items[0]})
+      //   })
+    YTSearch({key: API_KEY, term}, videos => {
+      this.setState({videos, selectedVideo: videos[0]});
+    })
+  }
+
+  handleChange = (value) => {
+    setTimeout( () => {
+      if( value === ''){
+        this.setState({ videos: [], selectedVideo: null });
+        return;
+      }
+
+      if( this.state.search ) {
+        this.videoSearch( value );
+      }
+
+      setTimeout( () => {
+        this.setState({ search: true });
+      }, 5000);
+
+    }, 2000);
+
+  };
+  
   render() {
     return (
       <div>
-        <div>
-          <h1>YTSearch<SearchOutlined /></h1>
-          <SearchBar/>
+        <div style={{"display":"flex", "justifyContent": "center","alignItems": "center"}}>
+          <h1 style={{"color":"#FF0000","marginLeft":"35px"}}>YouTube Search<SearchOutlined /></h1>
+          <SearchBar 
+            videos={ this.state.videos } 
+            video={ this.state.selectedVideo }
+            handleSearch={ (video) => { this.setState({ selectedVideo: this.state.videos[video], search: false })}}
+            onChange={ this.handleChange }
+          />
         </div>
       </div>
     )
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { AutoComplete, Button } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
-
-
+import { AutoComplete, Button } from 'antd';
+const {Option} = AutoComplete;
 
 export default class SearchBar extends Component {
+  state = {
+    videos: [],
+  }
+  componentDidUpdate( prevProps ) {
+    console.log(prevProps.video)
+    if( this.props.video && prevProps.video !== this.props.video ) {
+        this.setState({ videos: this.props.videos })
+    }
+  } 
+  onSelect = ( value, option ) => {
+    
+    let val = parseInt(option.key, 10); // parseInt(string, radix- he thap phan: 10, nhi phan-2)
+    this.props.handleSearch( val );
+  };
   render() {
     return (
-      <div>
+      <div style={{ "textAlign": "center", "padding": "35px" }}>
         <AutoComplete
           size={"large"}
           style={{ width: 400 }}
@@ -15,6 +28,8 @@ export default class SearchBar extends Component {
           onChange={ this.props.onChange }
           placeholder="Search Video"
         >
+        
+        { this.state.videos.map((video, index)  => <Option key={ index } value={video.snippet.title}>{ video.snippet.title }</Option> ) }
         </AutoComplete>
         <Button style={{"marginLeft": "5px"}} size="large"><SearchOutlined />Search</Button>
       </div>
